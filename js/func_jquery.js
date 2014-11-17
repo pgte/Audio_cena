ac129f8246024dc4bdce4308789da44a2e9baa63
mgteixeira <mgteixeira@gmail.com>
2014-11-17 17:35:52 +0000

    debug Js

--- js/func_jquery.js
+++ js/func_jquery.js
@@ -1,4 +1,6 @@
 (function(){
+
+//Define the function that will get the file in the buffer
 	var ac = new AudioContext(); 
 	var binj; 
 	var getbinj = new XMLHttpRequest(); 
@@ -9,13 +11,14 @@
 			binj = buffer; 	
 		}); 
 	}; 
-
+// Call the function to load the file into buffer
 	getbinj.send();
 
+//Call the function that will play the buffer 
 	$(document).ready(function(){
 		$(".bolas").click(function(){
 			var playSound = ac.createBufferSource(); 
-			playSound.buffer = binj; 
+//			playSound.buffer = binj; 
 			playSound.connect(ac.destination); 
 			playSound.noteOn(0); 
 		}); 
